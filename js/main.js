8b22b978a1bbe8dc8859c768a4cb83887e5d614e
auslum <ulises.a.rivero.garcia@gmail.com>
2025-09-22 19:30:39 -0600

    added dynamic list to .js file

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@ const links = [
     label: "Week1 notes",
     url: "/index.html"
   }
-]
+];
+
+const list = document.getElementById("linkList");
+links.forEach(link => {
+  const listItem = document.createElement("li");
+  const anchor = document.createElement("a");
+
+  anchor.textContent = link.label;
+  anchor.href = link.url;
+
+  listItem.appendChild(anchor);
+  list.appendChild(listItem);
+});
